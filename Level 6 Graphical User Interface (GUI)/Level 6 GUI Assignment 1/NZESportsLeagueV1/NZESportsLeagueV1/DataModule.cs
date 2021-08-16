27730e69f8afa4a6a3efec7fe6cd0ed0d0b48dd4
karanina <61443737+karanina@users.noreply.github.com>
2021-08-17 10:03:48 +1200

    transfer projects to portfolio

---
+++ Level 6 Graphical User Interface (GUI)/Level 6 GUI Assignment 1/NZESportsLeagueV1/NZESportsLeagueV1/DataModule.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace NZESportsLeagueV1
+{
+    public partial class DataModule : Form
+    {
+        public DataTable dtArena;
+        public DataTable dtEvent;
+        public DataTable dtCompetitor;
+        public DataTable dtChallenge;
+        public DataTable dtEntry;
+        public DataView arenaView;
+        public DataView eventView;
+        public DataView competitorView;
+        public DataView challengeView;
+
+
+        public DataModule()
+        {
+            InitializeComponent();
+            dsNZESL.EnforceConstraints = false;
+            daArena.Fill(dsNZESL);
+            daEvent.Fill(dsNZESL);
+            daCompetitor.Fill(dsNZESL);
+            daChallenge.Fill(dsNZESL);
+            daEntry.Fill(dsNZESL);
+            dtArena = dsNZESL.Tables["Arena"];
+            dtEvent = dsNZESL.Tables["Event"];
+            dtCompetitor = dsNZESL.Tables["Competitor"];
+            dtChallenge = dsNZESL.Tables["Challenge"];
+            dtEntry = dsNZESL.Tables["Entry"];
+            arenaView = new DataView(dtArena);
+            arenaView.Sort = "ArenaID";
+            eventView = new DataView(dtEvent);
+            eventView.Sort = "EventID";
+            competitorView = new DataView(dtCompetitor);
+            competitorView.Sort = "CompetitorID";
+            challengeView = new DataView(dtChallenge);
+            challengeView.Sort = "ChallengeID";
+            dsNZESL.EnforceConstraints = true;
+        }
+
+        public void UpdateArena()
+        {
+            daArena.Update(dtArena);
+        }
+
+        private void daArena_RowUpdated(object sender, OleDbRowUpdatedEventArgs e)
+        {
+            int newID = 0;
+            OleDbCommand idCMD = new OleDbCommand("SELECT @@IDENTITY", ctnNZESL);
+
+            if (e.StatementType == StatementType.Insert)
+            {
+                newID = (int)idCMD.ExecuteScalar();
+                e.Row["ArenaID"] = newID;
+            }
+        }
+
+        public void UpdateEvent()
+        {
+            daEvent.Update(dtEvent);
+        }
+
+        private void daEvent_RowUpdated(object sender, OleDbRowUpdatedEventArgs e)
+        {
+            int newID = 0;
+            OleDbCommand idCMD = new OleDbCommand("SELECT @@IDENTITY", ctnNZESL);
+
+            if (e.StatementType == StatementType.Insert)
+            {
+                newID = (int)idCMD.ExecuteScalar();
+                e.Row["EventID"] = newID;
+            }
+        }
+        public void UpdateChallenge()
+        {
+            daChallenge.Update(dtChallenge);
+        }
+
+        private void daChallenge_RowUpdated(object sender, OleDbRowUpdatedEventArgs e)
+        {
+            int newID = 0;
+            OleDbCommand idCMD = new OleDbCommand("SELECT @@IDENTITY", ctnNZESL);
+
+            if (e.StatementType == StatementType.Insert)
+            {
+                newID = (int)idCMD.ExecuteScalar();
+                e.Row["ChallengeID"] = newID;
+            }    
+        }
+
+        public void UpdateCompetitor()
+        {
+            daCompetitor.Update(dtCompetitor);
+        }
+
+        private void daCompetitor_RowUpdated(object sender, OleDbRowUpdatedEventArgs e)
+        {
+            int newID = 0;
+            OleDbCommand idCMD = new OleDbCommand("SELECT @@IDENTITY", ctnNZESL);
+
+            if (e.StatementType == StatementType.Insert)
+            {
+                newID = (int)idCMD.ExecuteScalar();
+                e.Row["CompetitorID"] = newID;
+            }
+        }
+
+        public void UpdateEntry()
+        {
+            daEntry.Update(dtEntry);
+        }
+
+       
+ 
+    }
+}
---
+++ Level 6 Graphical User Interface (GUI)/Level 6 GUI Assignment 1/NZESportsLeagueV1/NZESportsLeagueV1/MainForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NZESportsLeagueV1
+{
+    public partial class MainForm : Form
+    {
+        // This is the reference to the Data Module form which holds the data components.
+        private DataModule DM;
+
+        // This is the reference to the Arena Maintenance form.
+        private ArenaMaintenanceForm frmArenaMaintenance;
+
+        // This is the reference to the Event Maintenance form.
+        private EventMaintenanceForm frmEventMaintenance;
+
+        // This is the reference to the Challenge Maintenance form.
+        private ChallengeMaintenanceForm frmChallengeMaintenance;
+
+        // This is the reference to the Competitor Maintenance form.
+        private CompetitorMaintenanceForm frmCompetitorMaintenance;
+
+        // This is the reference to the Enter Competitor Into Challenge Form.
+        private EnterCompetitorIntoChallengeForm frmEnterCompetitorIntoChallenge;
+
+        // This is the reference to the Events Report form.
+        private EventsReportForm frmEventsReport;
+
+        // This is the reference to the Competitor Report form.
+        private CompetitorReportForm frmCompetitorReport;
+
+        // This creates the data module and loads the dataset.
+        private void MainForm_Load (object sender, EventArgs e)
+        {
+            DM = new DataModule();
+        }
+
+        // This is the constructor method.
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // This is a button click method which creates an ArenaMaintenanceForm object, if none exists, and
+        // passes it references to the DataModule and MainForm. 
+        private void btnArenaMaintenance_Click (object sender, EventArgs e)
+        {
+            if (frmArenaMaintenance == null)
+            {
+                frmArenaMaintenance = new ArenaMaintenanceForm(DM, this);
+            }
+            frmArenaMaintenance.ShowDialog();
+        }
+
+        // This is a button click method which creates an EventMaintenanceForm object, if none exists, and
+        // passes it references to the DataModule and MainForm. 
+        private void btnEventMaintenance_Click(object sender, EventArgs e)
+        {
+            if (frmEventMaintenance == null)
+            {
+                frmEventMaintenance = new EventMaintenanceForm(DM, this);
+            }
+            frmEventMaintenance.ShowDialog();
+        }
+
+        // This is a button click method which creates a ChallengeMaintenanceForm object, if none exists, and
+        // passes it references to the DataModule and MainForm. 
+        private void btnChallengeMaintenance_Click(object sender, EventArgs e)
+        {
+            if (frmChallengeMaintenance == null)
+            {
+                frmChallengeMaintenance = new ChallengeMaintenanceForm(DM, this);
+            }
+            frmChallengeMaintenance.ShowDialog();
+        }
+
+        // This is a button click method which creates a CompetitorMaintenanceForm object, if none exists, and
+        // passes it references to the DataModule and MainForm. 
+        private void btnCompetitorMaintenance_Click(object sender, EventArgs e)
+        {
+            if (frmCompetitorMaintenance == null)
+            {
+                frmCompetitorMaintenance = new CompetitorMaintenanceForm(DM, this);
+            }
+            frmCompetitorMaintenance.ShowDialog();
+        }
+
+        // This is a button click method which creates an EnterCompetitorIntoChallengeForm object, if none exists, and
+        // passes it references to the DataModule and MainForm. 
+        private void btnEnterCompetitorIntoChallenge_Click(object sender, EventArgs e)
+        {
+            if (frmEnterCompetitorIntoChallenge == null)
+            {
+                frmEnterCompetitorIntoChallenge = new EnterCompetitorIntoChallengeForm(DM, this);
+            }
+            frmEnterCompetitorIntoChallenge.ShowDialog();
+        }
+
+        // This is a button click method which creates an EventsReportForm object, if none exists, and
+        // passes it references to the DataModule and MainForm. 
+        private void btnEventsReport_Click(object sender, EventArgs e)
+        {
+            if (frmEventsReport == null)
+            {
+                frmEventsReport = new EventsReportForm(DM, this);
+            }
+            frmEventsReport.ShowDialog();
+        }
+
+        // This is a button click method which creates a CompetitorReportForm object, if none exists, and
+        // passes it references to the DataModule and MainForm. 
+        private void btnCompetitorReport_Click(object sender, EventArgs e)
+        {
+            if (frmCompetitorReport == null)
+            {
+                frmCompetitorReport = new CompetitorReportForm(DM, this);
+            }
+            frmCompetitorReport.ShowDialog();
+        }
+
+        // This is a button click method which closes the program when the user presses the Exit button.
+        private void btnExit_Click (object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
