77b3c35400c85acb0f3ebef4c70098f8cc1dc0c5
karanina <61443737+karanina@users.noreply.github.com>
2021-08-17 10:28:33 +1200

    gui update

---
+++ Level 6 GUI Programming/TowersOfHanoiiGame/TowersOfHanoi_v2/TowersOfHanoi_Starter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Towers_of_Hanoi
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
